8c44dfe384eade385a7a352a1f3bd0dcb73f7f6a
Elias Rajabi <91074307+Eliasrajabi@users.noreply.github.com>
2024-03-11 16:30:37 +0100

    assignment2

---
+++ src/views/AddressForm/FormInterface.tsx
@@ -0,0 +1,24 @@
+﻿export interface Country {
+    alpha2Code: string; //represent countries and regions.
+    name: string;
+    code: string;
+}
+
+export interface Address {
+    country: string;
+    zip: string;
+    city: string;
+    address1: string;
+    address2: string;
+    name: string;
+    phone: string;
+    email: string;
+    company: string;
+    vat: string;
+    countryCode: string;
+}
+export interface AddressFormState {
+    delivery: Address;
+    billing: Address;
+    isBillingDifferent: boolean;
+}
---
+++ src/views/AddressForm/FormValidation.tsx
@@ -0,0 +1,26 @@
+﻿
+export function isValidZip(zip: string): boolean {
+    return /^\d{4}$/.test(zip);
+}
+
+export function validateEmail(email: string): boolean {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+
+export function validateVAT(vat: string, country: string): boolean {
+    if (country === 'Denmark') {
+        const regex = /^\d{8}$/;
+        return regex.test(vat.trim());
+    }
+    return true;
+}
+
+export function validatePhoneNumber(phone: string, country: string): boolean {
+    if (country === 'Denmark') {
+        const regex = /^\d{8}$/;
+        return regex.test(phone.trim());
+    }
+    return true;
+}
